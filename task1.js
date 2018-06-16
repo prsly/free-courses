d89e1e7a95cf62a2d539668d4dec193c37686dba
Michael <i@prsly.ru>
2018-06-16 12:37:44 +0300

    add resolve of task1

---
+++ task1.js
@@ -0,0 +1,15 @@
+/* Task 1 */
+
+/* Put your code here */
+word = process.argv[2];
+excp = "РКНркн";
+
+for (i = 0; i< word.length; i++){
+    for (j = 0; j < excp.length; j++){
+        if (word.charAt(i) == excp.charAt(j)){
+            word = word.substring(0, i) + word.substring(i+1);
+            i--;
+        }
+    }
+}
+console.log(word);
