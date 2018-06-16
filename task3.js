c61b147c1def767d98290bd611f9760817c152bc
Michael <i@prsly.ru>
2018-06-16 13:08:14 +0300

    add solve for task 3

---
+++ task3.js
@@ -0,0 +1,14 @@
+/* Task 3 */
+
+/* Put your code here */
+a = process.argv[2];
+b = process.argv[3];
+
+while(b != 0){
+    temp = a;
+    a = b;
+    b = temp%b;
+}
+
+nod=Math.abs(a);
+console.log(nod);
